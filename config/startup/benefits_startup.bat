64f564e8d7ff91c094ffcfc2beaf6edf15d9f260
David A. Taylor <tayloredwebsites@me.com>
2017-01-22 10:30:23 -0500

    final fixes for Rails 4 version Dec 2016
    fixed monthly benefits report and specified ip address 0.0.0.0 (not new
    default of 127.0.0.1)

--- config/startup/benefits_startup.bat
+++ config/startup/benefits_startup.bat
@@ -2,4 +2,4 @@ rem benefits_startup.bat
 rem batch routine to start benefits in always up service manager
 path c:\progs\RailsInstaller\Ruby2.2.0\bin;c:\progs\RailsInstaller\Ruby1.9.3\bin;%path%
 cd c:\Sites\davis_bacon_401k
-bundle exec rails server -e production -p 4001
+bundle exec rails server -e production -p 4001 -b 0.0.0.0
